5a69648d02843dfabe3a80b356c771cc3dc9aecb
danielliu524 <47194494+danielliu524@users.noreply.github.com>
2021-05-30 18:40:32 +0800

    Functionality

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const DiscordJS = require("discord.js");
 require("dotenv").config()
 
-const client = new DiscordJS.Client()
+const client = new DiscordJS.Client();
+const getApp = (guildId) => {
+    const app = client.api.applications(client.user.id);
+    if(guildId) {
+        app.guilds(guildId);
+    }
+    return app;
+}
 
-client.on("ready", () => {
+client.on("ready", async () => {
     console.log("Bubble Wrap Bot Online");
+
+    const commands = await getApp().commands.get();
+    console.log(commands);
+    await getApp().commands.post({
+        data: {
+            name: "bubble",
+            description: "Makes a bubble wrap. Enjoy!",
+        }
+    })
+
+    client.ws.on("INTERACTION_CREATE", async (interaction) => {
+        const command = interaction.data.name.toLowerCase();
+
+        if(command === "bubble") {
+            client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        content: `||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||
+||)||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||(||
+||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||
+||)||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||(||
+||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||
+||)||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||(||
+||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||
+||)||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||(||
+||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||||()||`,
+                    }
+                }
+            })
+        }
+    })
 })
 
 client.login(process.env.TOKEN);
